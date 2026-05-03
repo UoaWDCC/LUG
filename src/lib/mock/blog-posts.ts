3ce8a2176e9b3261922c57abca702dae119687fd
James Mullane <132721344+jamesmullane05@users.noreply.github.com>
2026-05-04 00:53:55 +1200

    feat(rss): add blog rss feed (#35)
    created the dummy data function in the lib folder
    
    created the rss feed, which uses the feed import to create the feed.
    
    You will be able to test it by going to /blog/rss.xml, or you can drop
    the localhost:3000/blog/rss.xml
    
    You can test it locally on a software by downloading feedflow on
    microsoft appstore

---
+++ src/lib/mock/blog-posts.ts
@@ -0,0 +1,23 @@
+export type BlogItem = {
+  title: string;
+  slug: string;
+  description: string;
+  publishedAt: string;
+};
+
+export async function getMockBlogItems(): Promise<BlogItem[]> {
+  return [
+    {
+      title: "Example Blog Item",
+      slug: "example-blog-item",
+      description: "This is an example blog item.",
+      publishedAt: "2026-04-30",
+    },
+    {
+      title: "Another Update",
+      slug: "another-update",
+      description: "This is another update.",
+      publishedAt: "2026-04-28",
+    },
+  ];
+}
