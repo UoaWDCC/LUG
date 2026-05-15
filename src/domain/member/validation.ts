0fdc9cf003eb133bad72bd556e7ba1518b331362
Baconfu <lixiaochuan02@gmail.com>
2026-05-16 02:54:30 +1200

    feat(validation): completed registration validation

--- src/domain/member/validation.ts
+++ src/domain/member/validation.ts
@@ -1,5 +1,28 @@
 import { PotentialInvolvement } from "@/generated/prisma/enums";
 
+const validYearLevel = [
+  "FIRST_YEAR",
+  "SECOND_YEAR",
+  "THIRD_YEAR",
+  "FOURTH_YEAR",
+  "FIFTH_YEAR_OR_LATER",
+  "GRADUATED_WITHIN_2_YEARS",
+] as const;
+const validPotentialInvolvement = [
+  "ATTENDING",
+  "SPEAKING",
+  "EXECUTIVE",
+  "PROJECTS",
+] as const;
+const validLinuxSkillLevel = [
+  "NOTHING",
+  "AWARE_OF_EXISTENCE",
+  "BEGINNER_USER",
+  "REGULAR_USER",
+  "POWER_USER",
+  "CONTRIBUTOR",
+] as const;
+
 type RegistrationFormValidationError = {
   message: string;
 };
@@ -21,7 +44,6 @@ export function validateMemberRegistration(
 
   return { ok: true, data: toMemberRegistrationObject(parsedRegistrationForm) };
 }
-
 function validateFields(
   parsed: ParsedRegistrationFormSubmission,
 ):
@@ -30,10 +52,60 @@ function validateFields(
   if (!isValidEmail(parsed.email)) {
     return {
       fieldsValid: false,
-      error: { message: "Email invalid: please enter a valid email address" },
+      error: {
+        message: `Email invalid: '${parsed.email}', please enter a valid email address`,
+      },
     };
   }
+  if (
+    !validLinuxSkillLevel.includes(parsed.linuxSkillLevel as LinuxSkillLevel)
+  ) {
+    return {
+      fieldsValid: false,
+      error: {
+        message: `linuxSkillLevel property has invalid value: '${parsed.linuxSkillLevel}'`,
+      },
+    };
+  }
+  for (const option of parsed.potentialInvolvement) {
+    if (!validPotentialInvolvement.includes(option)) {
+      return {
+        fieldsValid: false,
+        error: {
+          message: `potentialInvolvement field contains a selection with invalid value: '${option}'`,
+        },
+      };
+    }
+  }
+
+  if (
+    parsed.isCurrentUoaStudent == "true" ||
+    parsed.isEligibleReturningUoaStudent == "true"
+  ) {
+    if (!validYearLevel.includes(parsed.yearLevel as YearLevel)) {
+      return {
+        fieldsValid: false,
+        error: {
+          message: `yearLevel property has invalid value: ${parsed.yearLevel}`,
+        },
+      };
+    }
+    if (!isValidUPI(parsed.upi!)) {
+      return {
+        fieldsValid: false,
+        error: {
+          message: `upi invalid: '${parsed.upi}', please enter a valid upi`,
+        },
+      };
+    }
+  }
+
   return { fieldsValid: true };
+}
+
+function isValidUPI(upi: string): boolean {
+  const upiRegex = /^[A-Za-z]{3,4}[0-9]{3}$/;
+  return upiRegex.test(upi);
 }
 
 function isValidEmail(email: string): boolean {
@@ -51,12 +123,15 @@ function hasRequiredFields(
   | { hasRequiredFields: true }
   | { hasRequiredFields: false; error: RegistrationFormValidationError } {
   //Base fields required by all registrations
+
   const requiredFields: string[] = [
     "firstName",
     "lastName",
     "email",
     "linuxSkillLevel",
     "potentialInvolvement",
+    "isEligibleReturningUoaStudent",
+    "isCurrentUoaStudent",
   ];
   const errorDisplayNames: string[] = [
     "First name",
@@ -64,25 +139,62 @@ function hasRequiredFields(
     "Email",
     "Linux skill level",
     "Potential involvement",
+    "Is eligible returning Uoa student",
+    "Is current Uoa student",
   ];
   for (const field of requiredFields) {
-    if (!(field in parsed) || !parsed[field]) {
+    if (!(field in parsed) || !parsed[field as keyof typeof parsed]) {
       const displayName = errorDisplayNames[requiredFields.indexOf(field)];
       return {
         hasRequiredFields: false,
-        error: { message: "${displayName} field is required" },
+        error: {
+          message: `'${displayName}' field is required, and must not be null or empty`,
+        },
       };
     }
   }
+
+  if (parsed.potentialInvolvement.length === 0) {
+    return {
+      hasRequiredFields: false,
+      error: {
+        message:
+          "At least one option for Potential Involvement must be selected",
+      },
+    };
+  }
+
+  //check if the value of isEligibleReturningUoaStudent and isCurrentUoaStudent is valid.
+  //It is a validity check(thus should be in validateFields function) but is required for the following checks so is moved here.
+  if (
+    !["true", "false"].includes(parsed.isEligibleReturningUoaStudent as string)
+  ) {
+    return {
+      hasRequiredFields: false,
+      error: {
+        message: `isEligibleReturningUoaStudent field has value: '${parsed.isEligibleReturningUoaStudent}' must either have value 'true' or 'false'`,
+      },
+    };
+  }
+  if (!["true", "false"].includes(parsed.isCurrentUoaStudent as string)) {
+    return {
+      hasRequiredFields: false,
+      error: {
+        message:
+          "isCurrentUoaStudent field must either have value 'true' or 'false'",
+      },
+    };
+  }
+
   // Further check for fields that are only required if specific conditions are met
   if (parsed.isEligibleReturningUoaStudent == "true") {
-    if (!parsed.upi) {
+    if (!("upi" in parsed) || !parsed.upi) {
       return {
         hasRequiredFields: false,
         error: { message: "upi field is required for Uoa students" },
       };
     }
-    if (!parsed.studentId) {
+    if (!("studentId" in parsed) || !parsed.studentId) {
       return {
         hasRequiredFields: false,
         error: { message: "Student ID field is required for Uoa students" },
@@ -104,24 +216,24 @@ function hasRequiredFields(
       "Year level",
     ];
     for (const field of requiredFieldsUoa) {
-      if (!(field in parsed) || !parsed[field]) {
+      if (!(field in parsed) || !parsed[field as keyof typeof parsed]) {
         const displayName =
           errorDisplayNamesUoa[requiredFieldsUoa.indexOf(field)];
         return {
           hasRequiredFields: false,
           error: {
-            message: "${displayName} field is required for Uoa students",
+            message: `'${displayName}' field is required for Uoa students, and must not be null or empty`,
           },
         };
       }
     }
   } else if (parsed.isCurrentUoaStudent == "false") {
-    if (!parsed.primaryAffiliation) {
+    if (!("primaryAffiliation" in parsed) || !parsed.primaryAffiliation) {
       return {
         hasRequiredFields: false,
         error: {
           message:
-            "Primary Affiliation field is required for non-Uoa candidates",
+            "'Primary Affiliation' field is required for non-Uoa candidates, and must not be null or empty",
         },
       };
     }
@@ -136,7 +248,7 @@ function toMemberRegistrationObject(
     return toConditionalReturningMember(parsedForm);
   } else if (parsedForm.isCurrentUoaStudent == "true") {
     return toCurrentUoaStudent(parsedForm);
-  } else if (parsedForm.isCurrentUoaStudent == "false") {
+  } else {
     return toNonCurrentUoaStudentMember(parsedForm);
   }
 }
@@ -148,7 +260,7 @@ function toNonCurrentUoaStudentMember(
     ...toBaseMember(parsed),
     isEligibleReturningUoaStudent: false,
     isCurrentUoaStudent: false,
-    primaryAffiliation: parsed.primaryAffiliation,
+    primaryAffiliation: parsed.primaryAffiliation!,
     ...(parsed.nonUoaExcerpt != null
       ? { nonUoaExcerpt: parsed.nonUoaExcerpt }
       : {}),
@@ -163,8 +275,8 @@ function toConditionalReturningMember(
   const returningMember: ConditionalReturningMember = {
     ...toBaseMember(parsed),
     isEligibleReturningUoaStudent: true,
-    upi: parsed.upi,
-    studentId: parsed.studentId,
+    upi: parsed.upi!,
+    studentId: parsed.studentId!,
   };
   return returningMember;
 }
@@ -176,11 +288,11 @@ function toCurrentUoaStudent(
     isEligibleReturningUoaStudent: false,
     isCurrentUoaStudent: true,
 
-    upi: parsed.upi,
-    studentId: parsed.studentId,
-    faculty: parsed.faculty,
-    programme: parsed.programme,
-    yearLevel: parsed.yearLevel,
+    upi: parsed.upi!,
+    studentId: parsed.studentId!,
+    faculty: parsed.faculty!,
+    programme: parsed.programme!,
+    yearLevel: parsed.yearLevel!,
   };
   return uoaMember;
 }
