42dcc7dbcb6bbd6422de19bfb9b1f0f062368532
James Mullane <132721344+jamesmullane05@users.noreply.github.com>
2026-05-21 17:41:07 +1200

    test(setup): add testing configuration (#49)
    * test(setup): added vitest & playwright configuration
    
    * test(blogposts): refactoring blogposts and added testcase
    
    * fix(testing config): prettier fix
    
    * refactor(testing setup): adjusting config
    
    * fix(pnpm-lock): fixed broken pnpm lock
    
    * refactor(route): removing blog route
    
    * feat(setup): changing the ci and minor changes
    
    * refactor(testing): chaning the folder name
    
    * fix: set correct package versions, change CI and check to run unit tests before build
    
    ---------
    
    Co-authored-by: William Tay <williamthomastay@gmail.com>

---
+++ vitest.setup.ts
@@ -0,0 +1,7 @@
+/* 
+Global Vitest setup file.
+Runs before every test suite and is used to configure
+shared testing utilities, mocks, and custom matchers.
+*/
+
+import "@testing-library/jest-dom/vitest";
